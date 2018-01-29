38c599803c6d0e7146b2547521532a11e1bb5349
Alex Bymoen <a.bymoen@hotmail.com>
2018-01-28 21:37:33 -0700

    Kind of working game

---
+++ javascript/multiTargetGame/Driver.js
@@ -0,0 +1,33 @@
+var targets = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight/1.5);
+    background(150);
+    for(var i=0; i<20; i++) {
+        targets[i] = new Target();
+        targets[0].isCurrentTarget = true;
+        targets[i].show();
+    }
+}
+
+function draw() {
+    if(mouseIsPressed) {
+        for(var i=0; i<targets.length; i++) {
+            if(targets[i].targetHit() && targets[i].isCurrentTarget) {
+                clear();
+                background(150);
+                targets[i].isCurrentTarget = false;
+                targets[(i+1)%10].isCurrentTarget = true;
+                targets[i].shootTarget();
+                drawAllTargets();
+            }
+        }
+    }
+}
+
+function drawAllTargets() {
+    for(var i=0; i<targets.length; i++) {
+        targets[i].show();
+    }
+}
+
