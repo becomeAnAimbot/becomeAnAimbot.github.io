43ae42bc5a000b4c282413e3c2978447f88b086e
Alex Bymoen <a.bymoen@hotmail.com>
2018-02-06 07:06:35 -0700

    Added start button

--- javascript/multiTargetGame/Driver.js
+++ javascript/multiTargetGame/Driver.js
@@ -1,17 +1,20 @@
 var targets = [];
+var gameStarted = false;
 
 function setup() {
     var canv = createCanvas(windowWidth, windowHeight/1.5);
     canv.parent('gameContainer');
     background(150);
-    for(var i=0; i<20; i++) {
-        targets[i] = new Target();
-        targets[0].isCurrentTarget = true;
-        targets[i].show();
-    }
+    startButton = createButton("START GAME");
+    startButton.addClass("startButton");
+    startButton.position(windowWidth/2 - startButton.width, windowHeight/2.5 - startButton.height);
+    startButton.mousePressed(startGame);
 }
 
 function draw() {
+    if(!gameStarted) {
+        //Nothing for now      
+    }
     if(mouseIsPressed) {
         for(var i=0; i<targets.length; i++) {
             if(targets[i].targetHit() && targets[i].isCurrentTarget) {
@@ -32,3 +35,27 @@ function drawAllTargets() {
     }
 }
 
+function createTargets() {
+    for(var i=0; i<20; i++) {
+        targets[i] = new Target();
+        targets[0].isCurrentTarget = true;
+        targets[i].show();
+    }
+}
+
+function startGame() {
+    startButton.hide();
+    countDown = createElement("h1", counter);
+    countDown.position(windowWidth/2, windowHeight/2);
+    var counter = 3;
+    var cd = setInterval(function() {
+        if(counter == 0) {
+            clearInterval(cd);
+            countDown.hide();
+            createTargets();
+            gameStarted = true;
+        }
+        countDown.html(counter);
+        counter--;
+    }, 1000);
+}
