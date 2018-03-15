f4bfe41fd3c07a0bdaa07c74301b557b61083ef7
Alex Bymoen <a.bymoen@hotmail.com>
2018-03-14 18:19:11 -0600

    Added title images

--- javascript/mainScreen/Driver.js
+++ javascript/mainScreen/Driver.js
@@ -1,16 +1,16 @@
 var mainSketch = function(p) {
-    
+
     ambientTargets = [];
-    
+
     p.setup = function() {
         canv = p.createCanvas(p.windowWidth, p.windowHeight);
         p.background(150);
         canv.style("z-index","-2");
         canv.addClass("mainScreenBackground");
         canv.position(0,0);
-        
+
         p.mainScreenHtml();
-        
+
         for(let i = 0; i < 25; i++) {
             t = new Target(p, p.windowWidth, p.windowHeight);
             t.randomPlacement();
@@ -20,50 +20,51 @@ var mainSketch = function(p) {
             ambientTargets.push(t);
         }
     };
-    
+
     p.draw = function() {
-        p.background("#FFFFFF"); 
+        p.background("#FFFFFF");
         for(let i = 0; i < ambientTargets.length; i++) {
             ambientTargets[i].show();
             ambientTargets[i].checkBoundaryHits(ambientTargets[i]);
             ambientTargets[i].move();
         }
     };
-    
+
     p.mainScreenHtml = function() {
         gameCont = p.createElement("div","");
         gameCont.id("bodyContainer");
-        
-        mainTitle = p.createElement("h1","Become An Aimbot");
+
+        mainTitle = p.createElement("img");
+        mainTitle.attribute("src", "images/large_title.png");
         mainTitle.parent(gameCont);
         mainTitle.addClass("mainTitle");
-        
+
         gamesList = p.createElement("div","");
         gamesList.parent(gameCont);
         gamesList.addClass("mainGamesList");
         gamesList.style("background","#FFFFFF");
-        
+
         gameOne = p.createElement("h2","Priority Target Practice");
         gameOne.parent(gamesList);
         gameOne.addClass("listGame");
         gameOne.attribute("onclick","startPriorityGame()");
-        
+
         gameTwo = p.createElement("h2","Priority Target Practice");
         gameTwo.parent(gamesList);
         gameTwo.addClass("listGame");
         gameTwo.attribute("onclick","startPriorityGame()");
-        
+
         gameThree = p.createElement("h2","Priority Target Practice");
         gameThree.parent(gamesList);
         gameThree.addClass("listGame");
         gameThree.attribute("onclick","startPriorityGame()");
-        
+
         gameFour = p.createElement("h2","Priority Target Practice");
         gameFour.parent(gamesList);
         gameFour.addClass("listGame");
         gameFour.attribute("onclick","startPriorityGame()");
     }
-    
+
     p.windowResized = function() {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
         for(let i = 0; i< ambientTargets.length; i++) {
