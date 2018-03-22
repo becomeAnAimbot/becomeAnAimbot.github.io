2a798524b6f0658bb6cffe004e2d885b53d71b6a
abymoen <a.bymoen@hotmail.com>
2018-03-22 19:17:26 +0000

    Moved to private server

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(6969, function(){
+  console.log('listening on 167.99.105.82:6969:');
+});
