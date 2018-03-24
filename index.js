86a206ae6c1828712eb354c914833420240dc78f
Alex <alex4d@live.ca>
2018-03-23 18:21:29 -0600

    First commit

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(6969, function(){
-  console.log('listening on 167.99.105.82:6969:');
+http.listen(6969, function() {
+    console.log('listening on 167.99.105.82:6969:');
 });
