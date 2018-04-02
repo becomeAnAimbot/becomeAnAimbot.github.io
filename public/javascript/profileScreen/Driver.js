1a4456900c7f53f32e0c978108e620a1a33e72b5
Alex Bymoen <a.bymoen@hotmail.com>
2018-04-01 21:40:08 -0600

    Fix function name

--- public/javascript/profileScreen/Driver.js
+++ public/javascript/profileScreen/Driver.js
@@ -125,7 +125,7 @@ var profileSketch = function(p) {
     hiddenDeleteInput.parent(deleteForm);
     hiddenDeleteInput.attribute("style","display:none;");
     hiddenDeleteInput.attribute("name", "func");
-    hiddenDeleteInput.attribute("value", "deleteAccount");
+    hiddenDeleteInput.attribute("value", "deleteUser");
 
     usernameDelInput = p.createElement("input","");
     usernameDelInput.parent(deleteForm);
