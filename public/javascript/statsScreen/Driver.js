51507a2d5c7caf908486d9b16c26f3af66e202fb
aebymoen <a.bymoen@hotmail.com>
2018-04-04 17:32:41 -0600

    Chart works now

--- public/javascript/statsScreen/Driver.js
+++ public/javascript/statsScreen/Driver.js
@@ -4,7 +4,6 @@ var statsSketch = function(p) {
 
     p.setup = function() {
         canv = p.createCanvas(p.windowWidth, p.windowHeight);
-        p.background(150);
         canv.style("z-index","-2");
         canv.addClass("mainScreenBackground");
         canv.position(0,0);
@@ -28,17 +27,6 @@ var statsSketch = function(p) {
         mainTitle.parent(gameCont);
         mainTitle.addClass("mainTitle");
 
-        statsBox = p.createElement("div","");
-        statsBox.parent(gameCont);
-        statsBox.addClass("statsBox");
-        statsBox.style("background","FFFFFF");
-
-        chart = p.createElement("div","");
-        chart.id("chart");
-        chart.addClass("with-3d-shadow with-transitions");
-        chart.addClass("chartHeight")
-        chart.parent(statsBox);
-
         lineParent = p.createElement("div", "");
         lineParent.attribute("id","lineCont");
         lineParent.parent(gameCont);
@@ -48,7 +36,6 @@ var statsSketch = function(p) {
         lineChart.parent(lineParent);
 
         p.samplebar(lineChart);
-        //p.generateLineGraph();
     };
 
     p.samplebar = function() {
@@ -75,62 +62,6 @@ var statsSketch = function(p) {
             ambientTargets[i].updateTargetBounds(p.windowWidth, p.windowHeight);
             ambientTargets[i].height = p.windowWidth/50;
             ambientTargets[i].width = p.windowWidth/50;        }
-    };
-
-    //TODO Replace with SQL Queries for User Statistics
-    p.generateRandomData = function() {
-        var accuracy1 = [];
-        var accuracy2 = [];
-
-        for (var i = 0; i < 100; i++){
-            random = Math.floor((Math.random() * 100) + 1);
-            accuracy1.push({x: i, y: random/100})
-        }
-
-        for (var i = 0; i < 100; i++){
-            random = Math.floor((Math.random() * 100) + 1);
-            accuracy2.push({x: i, y: random/100})
-        }
-
-        return [
-            {
-                key: "Priority Target Practice",
-                area: false,
-                values: accuracy1
-            },
-            {
-                key: "Game 2",
-                area: false,
-                values: accuracy2
-            }
-        ];
-
-        //return lineData;
-    };
-
-    p.generateLineGraph = function() {
-        nv.addGraph(function() {
-
-            var chart = nv.models.lineWithFocusChart();
-            chart.brushExtent([50,70]);
-
-            chart.xAxis.tickFormat(d3.format(',f')).axisLabel("# of Games Played");
-            chart.x2Axis.tickFormat(d3.format(',f'));
-
-            chart.yAxis.axisLabel("Accuracy");
-            chart.yTickFormat(d3.format(',.2f'));
-
-            chart.useInteractiveGuideline(true);
-            chart.yDomain([0,1]);
-
-            d3.select('#chart')
-                .append('svg')
-                .datum(p.generateRandomData())
-                .call(chart);
-
-            nv.utils.windowResize(chart.update);
-            return chart;
-        });
     };
 
 };
