515081463fb6fe764dee8d7b36c95403943be1b6
Alex Bymoen <a.bymoen@hotmail.com>
2018-04-08 22:42:42 -0600

    Adding table

--- public/javascript/leaderboard/Driver.js
+++ public/javascript/leaderboard/Driver.js
@@ -6,7 +6,8 @@ var leaderBoardSketch = function(p) {
       canv.addClass("mainScreenBackground");
       canv.position(0,0);
       p.leaderboardHtml();
-      p.requestLeaderboards();
+      gameBoards = p.requestLeaderboards();
+      p.createLeaderboards(gameBoards);
     };
 
     p.draw = function() {
@@ -30,13 +31,52 @@ var leaderBoardSketch = function(p) {
         leaderTitle.attribute("id","leaderboardTitle");
     }
 
+    p.createLeaderboards = function(gameBoards) {
+      fadeBoardCont = p.createElement("div","");
+      fadeBoardCont.attribute("class","leaderboardCont");
+      fadeBoardCont.parent(gameCont);
+
+      fadeTable = p.createElement("table","");
+      fadeTable.attribute("id","fadeAwayTable");
+      fadeTable.attribute("class","leaderTable");
+      fadeTable.parent(fadeBoardCont);
+
+      fadeHeaders = p.createElement("tr","");
+      fadeHeaders.parent(fadeTable);
+
+      p.createElement("th","Rank").parent(fadeHeaders);
+      p.createElement("th","Name").parent(fadeHeaders);
+      p.createElement("th","Effectiveness").parent(fadeHeaders);
+      p.createElement("th","Accuracy").parent(fadeHeaders);
+      p.createElement("th","Shots Taken").parent(fadeHeaders);
+
+      p.fillFadeData(gameBoards);
+    }
+
+    p.fillFadeData = function(gameBoards) {
+      for(i=0; i<gameBoards.boards.length; i++) {
+        row = p.createElement("tr","");
+        row.parent(fadeTable);
+        rank = p.createElement("td", i);
+        rank.parent(fadeTable);
+        name = p.createElement("td", gameBoards.boards[i].username);
+        name.parent(fadeTable);
+        effect = p.createElement("td", gameBoards.boards[i].effect);
+        effect.parent(fadeTable);
+        acc = p.createElement("td", (gameBoards.boards[i].hits / (gameBoards.boards[i].hits + gameBoards.boards[i].misses)).toFixed(3));
+        acc.parent(fadeTable);
+        taken = p.createElement("td", gameBoards.boards[i].hits + gameBoards.boards[i].misses);
+        taken.parent(fadeTable);
+      }
+    }
+
     p.requestLeaderboards = function() {
       var xhttp = new XMLHttpRequest();
       xhttp.open("POST", "/", true);
       xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          leaderboardInfo = JSON.parse(this.responseText).boards;
+          return JSON.parse(this.responseText).boards;
         }
       };
       xhttp.send(`func=getLeadersboards`);
